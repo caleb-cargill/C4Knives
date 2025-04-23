537277e9c545e142ace022f4e606f378fc5ec4c5
Caleb Cargill <cargilch@mail.uc.edu>
2025-04-22 21:27:13 -0400

    Add image to background of hero section

--- client/src/pages/HomePage.js
+++ client/src/pages/HomePage.js
@@ -118,8 +118,10 @@ const HomePage = () => {
   return (
     <div className="min-h-screen">
       {/* Hero Section */}
-      <section className="bg-gradient-to-r from-background to-secondary text-text py-20">
-        <div className="container mx-auto px-4">
+      <section className="relative bg-cover bg-center text-text py-20 bg-no-repeat" style={{ backgroundImage: "url('https://scontent-ord5-2.xx.fbcdn.net/v/t39.30808-6/473804028_1115350350373131_5327880234059863841_n.jpg?_nc_cat=101&ccb=1-7&_nc_sid=cc71e4&_nc_ohc=ujzrOapqUuMQ7kNvwGLLYJY&_nc_oc=AdkLQLeAtisg52dlHcWSLgvy3Xyyp3XEcbQBtJilihkOPz3tDodr0RL7B9rTnP6VnuF_sBcU2Vs9bswPx2O-qprn&_nc_zt=23&_nc_ht=scontent-ord5-2.xx&_nc_gid=RCYi3KCQ2cN8G5vUd56f8g&oh=00_AfGqzxC6D_iaSsMFkZ08NrzdgE_MgjZy5nFuRPUnBxaubg&oe=680E0848')" }}>
+        <div className="absolute inset-0 bg-black bg-opacity-70"></div>
+
+        <div className="container mx-auto px-4 relative z-10">
           <motion.div 
             className="max-w-2xl"
             initial={{ opacity: 0, y: 20 }}
@@ -160,7 +162,7 @@ const HomePage = () => {
             <div className="grid grid-cols-1 md:grid-cols-2 gap-8">
               <div>
                 <p className="text-5xl font-bold text-primary mb-4">{knifeCounter}+</p>
-                <p className="text-xl text-muted">Total Knives Forged</p>
+                <p className="text-xl text-muted">Total Knives Crafted</p>
               </div>
               <div>
                 <p className="text-5xl font-bold text-primary mb-4">{totalKnives}+</p>
